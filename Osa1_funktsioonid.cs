4946c4f6c437b5fc36c2b47d9eb01ef7f4fe33c2
opilane <opilane@HP-CZC2349HV2>
2025-09-05 11:39:50 +0300

    05.09

--- Osa1_funktsioonid.cs
+++ Osa1_funktsioonid.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,8 +11,124 @@ namespace TARpv24_C_
     {
         public static float Kalkulaator(float arv1, float arv2)
         {
-            float k=arv1 * arv2;
+            float k = arv1 * arv2;
             return k;
         }
+        public static string Kuu_nimetus(int kuu_nr)
+        {
+            string kuu = "";
+            switch (kuu_nr)
+            {
+                case 1: kuu = "Jaanuar"; break;
+                case 2: kuu = "Veebruar"; break;
+                case 3: kuu = "Märts"; break;
+                case 4: kuu = "Aprill"; break;
+                case 5: kuu = "Mai"; break;
+                case 6: kuu = "Juuni"; break;
+                case 7: kuu = "Juuli"; break;
+                case 8: kuu = "August"; break;
+                case 9: kuu = "September"; break;
+                case 10: kuu = "Oktoober"; break;
+                case 11: kuu = "Novemberr"; break;
+                case 12: kuu = "Detsember"; break;
+                default:
+                    kuu = "Viga";
+                    break;
+            }
+            return kuu;
+        }
+        public static string Hooaeg(int kuu_nr)
+        {
+            string hoo = "";
+            if (kuu_nr == 1 || kuu_nr == 2 || kuu_nr == 12) // && - and, || - or
+            {
+                hoo = "Talv";
+            }
+            else if (kuu_nr > 2 && kuu_nr < 6)
+            {
+                hoo = "Kevad";
+            }
+            else if (kuu_nr > 5 && kuu_nr < 9)
+            {
+                hoo = "Suvi";
+            }
+            else if (kuu_nr > 8 && kuu_nr < 12)
+            {
+                hoo = "Kevad";
+            }
+            else
+            {
+                hoo = "Viga";
+            }
+            return hoo;
+        }
+        public static string Pilet(int vanus)
+        {
+            string pilet = "";
+            if (vanus < 6 && vanus > 1)
+            {
+                pilet = "Tasuta";
+            }
+            else if (vanus > 5 && vanus < 15)
+            {
+                pilet = "Lastepilet";
+            }
+            else if (vanus > 14 && vanus < 66)
+            {
+                pilet = "Täispilet";
+            }
+            else if (vanus > 65 && vanus < 100)
+            {
+                pilet = "Sooduspilet";
+            }
+            else
+            {
+                pilet = "Viga andmetega";
+            }
+            return pilet;
+        }
+        public static string Nimi(string nimi, string nimi2)
+        {
+            string nimi_ = "";
+            if (nimi.ToLower() == "sasha" && nimi2.ToLower() == "mariia" || nimi.ToLower() == "mariia" && nimi2.ToLower() == "sasha")
+            {
+                nimi_ = "Te olete täna pinginaabrid.";
+            }
+            else
+            {
+                nimi_ = "Te ei ole täna pinginaabrid.";
+            }
+            return nimi_;
+
+        }
+        public static float Pind(float arv1, float arv2)
+        {
+            float pild = arv1 * arv2;
+            return pild;
+        }
+        public static float Hind(float m, float p)
+        {
+            float hind = m * p;
+            return hind;
+        }
+        public static float Summa(float arv)
+        {
+            float summa = arv / 0.7f;
+            return summa;
+        }
+        public static string Temperatuut(float arv)
+        {
+            string a = "";
+            if (arv > 18)
+            {
+                a = "Temperatuur on ruumi jaoks liiga kõrge.";
+            }
+            else
+            {
+                a = "Temperatuur on korras.";
+            }
+            return a;
+        }
+        public static string Pikk
     }
 }
--- Startclass.cs
+++ Startclass.cs
@@ -6,32 +6,154 @@ namespace TARpv24_C_
     {
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
+            //2. Osa
+            //Random rnd = new Random();
+            //int kuu_number=rnd.Next(1, 12);
+            //string nimetus = Osa1_funktsioonid.Kuu_nimetus(kuu_number);
+            //Console.WriteLine($"Nr: {kuu_number} - {nimetus}");
 
-            Console.BackgroundColor = ConsoleColor.Magenta;
-            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            //Console.WriteLine("Kas sa tahad dekodeerida arv -> nimetusse?");
+            //string vastus=Console.ReadLine();
+            //if (vastus.ToLower() != "jah")
+            //{
+            //    Console.WriteLine("Ei taha, siis ei taha");
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        Console.WriteLine("Nr: ");
+            //        kuu_number = int.Parse(Console.ReadLine());
+            //        Console.WriteLine(Osa1_funktsioonid.Hooaeg(kuu_number));
+            //    }
+            //    catch (Exception e)
+            //    {
 
-            Console.WriteLine("Tere tulemast! Mis on sinu nimi?");//выводит текст на консоль
-            string tekst = Console.ReadLine();//функция спрашивает что-то у пользователя
-            Console.WriteLine($"{tekst}, Rõõm näha!");// $ = f
-            int a = 1000; // целые числа
-            char taht = 'A';
-            Console.Write($"Esimene arv on {a}, Sisesta b = ... ");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine("Esimene arv on {0}, Sisesta {1}. Summa on {2}", a, b, a + b);
-            Console.WriteLine("Ujukomaarv");
-            double d = double.Parse(Console.ReadLine()); // десятичные
-            Console.WriteLine(d);
-            float f= float.Parse(Console.ReadLine());
-            Console.WriteLine(f);
-            bool t = false; // bool
+            //        Console.WriteLine(e);
+            //    }
+            //}
 
-            Random random = new Random(); // random
-            a = random.Next(-100,200);
-            Console.WriteLine(a);
+            // 1 Ülesanne
 
-            float vastus=Osa1_funktsioonid.Kalkulaator(f, a);
-            Console.WriteLine($"arv1 = {f}, arv2 = {a}. Kalkulaatori tulemus: {vastus}");
+            //Console.WriteLine("Mis su nimi on?");
+            //string vastus = Console.ReadLine();
+            //    if (vastus.ToLower() == "juku")
+            //    {
+            //        Console.WriteLine("Me läheme täna kinno. Kui vana sa oled?");
+            //        try
+            //        {
+            //            int vanus = int.Parse(Console.ReadLine());
+            //            Console.WriteLine(Osa1_funktsioonid.Pilet(vanus));
+            //        }
+            //        catch (Exception l)
+            //        {
+            //            Console.WriteLine(l );
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Me ei lähe täna kinno.");
+            //    }
+
+            ////2 Ülesanne
+            //Console.WriteLine("1.Mis su nimi on?");
+            //string nimi = Console.ReadLine();
+            //Console.WriteLine("2.Mis su nimi on?");
+            //string nimi2 = Console.ReadLine();
+            //Console.WriteLine(Osa1_funktsioonid.Nimi(nimi, nimi2));
+
+            ////3 Ülesanne
+            //Console.WriteLine("Kui pikk on sinu tuba? (vastus meetrites)");
+            //try
+            //{
+            //    float pikk = float.Parse(Console.ReadLine());
+            //    Console.WriteLine("Kui lai on sinu tuba ? (vastus meetrites)");
+            //    try
+            //    {
+            //        float lai = float.Parse(Console.ReadLine());
+            //        float pild = Osa1_funktsioonid.Pind(pikk, lai);
+            //        Console.WriteLine($"Sinu toa pindala: {pild}");
+            //        Console.WriteLine("Kas sa tahad remonti teha?");
+            //        string v = Console.ReadLine();
+            //        if (v.ToLower() != "ei")
+            //        {
+            //            Console.WriteLine( "Kui palju maksab ruutmeeter?");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("Tšao!");
+            //        }
+            //        try
+            //        {
+            //            float r = float.Parse(Console.ReadLine());
+            //            Console.WriteLine($"Põranda vahetamise hind: {Osa1_funktsioonid.Hind(r, pild)}");
+            //        }
+            //        catch (Exception p)
+            //        {
+            //            Console.WriteLine(p);
+            //        }
+            //    }
+            //    catch (Exception j)
+            //    {
+            //        Console.WriteLine(j);
+            //    }
+            //}
+            //catch (Exception k)
+            //{
+            //    Console.WriteLine(k);
+            //}
+
+            // 4 Ülesanne
+            Random rnd = new Random();
+            int arv=rnd.Next(1, 100);
+            Console.WriteLine($"Hind 30% soodustusega: {arv}, Algne hind: {Osa1_funktsioonid.Summa(arv)}");
+
+            //5 Ülesanne
+            Console.WriteLine("Mis temperatuur praegu on?");
+            try
+            {
+                float tem = float.Parse(Console.ReadLine());
+                Console.WriteLine(Osa1_funktsioonid.Temperatuut(tem));
+            }
+            catch (Exception ü)
+            {
+                Console.WriteLine(ü);
+            }
+
+            //6 Ülesanne
+            //Спроси рост человека и сообщи, низкий он, средний или высокий (границы установи сам).
+
+            //Спроси у человека рост и пол и сообщи, низкий он, средний или высокий(несколько блоков условий могут быть вложенными).
+
+            //Спроси у человека в магазине отдельно, хочет ли он купить молоко, булку, хлеб. Сложи цены и сообщи, сколько стоят все купленные продукты.
+            
+            //1. Osa
+            //Console.OutputEncoding = Encoding.UTF8;
+
+            //Console.BackgroundColor = ConsoleColor.Magenta;
+            //Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            //Console.WriteLine("Tere tulemast! Mis on sinu nimi?");//выводит текст на консоль
+            //string tekst = Console.ReadLine();//функция спрашивает что-то у пользователя
+            //Console.WriteLine($"{tekst}, Rõõm näha!");// $ = f
+            //int a = 1000; // целые числа
+            //char taht = 'A';
+            //Console.Write($"Esimene arv on {a}, Sisesta b = ... ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Esimene arv on {0}, Sisesta {1}. Summa on {2}", a, b, a + b);
+            //Console.WriteLine("Ujukomaarv");
+            //double d = double.Parse(Console.ReadLine()); // десятичные
+            //Console.WriteLine(d);
+            //float f= float.Parse(Console.ReadLine());
+            //Console.WriteLine(f);
+            //bool t = false; // bool
+
+            //Random random = new Random(); // random
+            //a = random.Next(-100,200);
+            //Console.WriteLine(a);
+
+            //float vastus=Osa1_funktsioonid.Kalkulaator(f, a);
+            //Console.WriteLine($"arv1 = {f}, arv2 = {a}. Kalkulaatori tulemus: {vastus}");
         }
-    }
+        }
 }
